2645f2f82d069a6e43ff68f11a18ee888847ed46
icemanovich <icemanovich@gmail.com>
2016-07-19 23:08:08 +0300

    Add Ajax JS methods

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,60 @@
 $(document).ready(function() {
 
     App = {
-        make_order: function(data){
-            console.log('111111')
+        /**
+         * Make an order AJAX request
+         * @param date
+         * @param user_id
+         * @param target_id
+         * @param btn
+         */
+        make_order: function(date, user_id, target_id, btn){
+
+            console.log('make order for ', arguments);
+
+            $.ajax({
+                url: '/order',
+                method: 'post',
+                data: {
+                    date: date,
+                    user_id: user_id,
+                    target_id: target_id,
+                    token: CSRF_TOKEN
+                },
+
+                success: function(data) {
+                    $('.results').html(data);
+
+                    btn.toggleClass('btn-primary', false);
+                    btn.attr('available', 'false');
+                    alert('Заявка успешно обработана. Время приёма: ' + date)
+                },
+                error: function(data){
+
+                    console.log('ERROR', data);
+
+
+                    btn.toggleClass('btn-primary', false);
+                    btn.attr('available', 'false');
+
+                    alert(data.responseJson.data);
+                }
+            });
         }
     };
 
-    // $("div").click(function() {
-    //     alert("Hello, world!");
-    // });
+
+    /**
+     * Handler for button click
+     */
+    $("button.make_order").click(function(data) {
+
+        if ($(this).attr('available').toLowerCase() == 'true') {
+            $args = $(this).attr('data').split(':');
+            App.make_order($args[0], $args[1], $args[2], $(this));
+        } else {
+            alert('Время уже зарезервировано. Выберите другое.');
+        }
+    });
 
 });
